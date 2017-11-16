fdcf6bf7beb30d4bb8e21681cdb05fc1a939bfe3
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-11-15 16:43:08 -0800

    final before debug

--- app.js
+++ app.js
@@ -1,62 +1,61 @@
-'use strict'
-
 'use strict';
+var stores = [];
 
-var tblElement = document.getElementById('main-tbl');
-var tbodyElement = document.createElement('tbody');
-tbodyElement.className = 'tbl-class';
-tbodyElement.id = 's-tbl-body';
-tblElement.appendChild(tbodyElement);
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-// Create a Row
-var trElement = document.createElement('tr');
-
-// Create a series of TDs, and provide each with content
-// append each TD to the TR
-for(var i = 0; i < 4; i++) {
-  var tdEl = document.createElement('td');
-  tdEl.textContent = i;
-  trElement.appendChild(tdElement);
+function store (name, min, max, avg) {
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  stores.push(this);
 }
+console.log('preoperty vale setup', store);
 
-// Append the TR to the TBody
-tbodyElement.appendChild(trElement);
+new store ('1st and Pike', 23, 65, 6.3);
+new store ('SeaTac', 3, 24, 1.2);
+new store ('Capitol Hill', 20, 38, 2.3);
+new store ('',2, 26, 1.2);
 
-// var firstAndPike = {
-//   name: '1st And Pike',
-//   minCustomers: 23,
-//   maxCustomers: 65,
-//   averageCookieSale: 6.3,
-//   dayZero: 0,//where the addition begins with adding total
-//   custPerHr: function () {
-//     return Math.random () * (this.maxCustomers - this.minCustomers) + this.minCustomers;
-//   },
-//   storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-//   averageCookiePerHour: function () {
-//     // var empty = [];
-//     for (var i = 0; i < this.storeHours.length; i++) {
-//       // jesus jelp with total function
-//       // rendor: function () //rendor allows you to display javascript data in html
-//       //if (i <15) {
-//       //end help
-//       var cookieSoldPerHr =(Math.round(this.averageCookieSale * this.custPerHr()));
-//       this.dayZero += cookieSoldPerHr;
-//       console.log ('cookies sold per hour', this.dayZero);
-//       var list = document.getElementsByTagName('ul')[0];
-//       var newItemLast = document.createElement('li');
-//       var newTextLast = //the next code below is to display li
-//       document.createTextNode(this.storeHours[i] + ': ' + cookieSoldPerHr);
-//       newItemLast.appendChild(newTextLast);
-//       list.appendChild(newItemLast);
-//       // } else { //more of jesus + kowsers help
-//       //   var newList = document.createElement('li');
-//       //   var newContent = document.createTextNode('Total: ' + this.dayZero );
-//       //   newItemLast = document.createElement('li2');
-//       //   newTextLast = document.createTextNode() //this pulls from 'i' in node
-//       //
-//     }
-//   }
-// };
-// //return empty;
-// firstAndPike.averageCookiePerHour();
-// console.log(firstAndPike);
+store.prototype.randomCust = function (){
+  return Math.round(this.avg * this,randomCust());
+};
+stores [0].cookieCount();
+console.log ('store value setup', new store);
+store.header = function () {
+  var tblEl = document.getElementById('table');
+  var thead = document.createElement('thead');
+  tblEl.appendChild('thead');
+  var reEl = document.createElement('tr');
+  tdEl.textContent = '';
+  trE1.appendChild(tdEl);
+  for (var i = 0; i <15; i++) {
+    tdEl = document.createElement('td');
+    var time = hours[i];
+    tdEl.textContent=time;
+    trEl.a[[endChild(tdE1);
+  }
+};
+store.header();
+store.prototype.tableContent = function () {
+  var cookiesDay = 0;
+  var tbEl = document.getElementById('table');
+  var tbody = document.createElement('tr');
+  tblEl.appendChild(tbody);
+  var trEl = document.createElement('tr');
+  tbody.appendChild(trEl);
+  var tdEl = document.createElement('td');
+  tdEl.textcontent = stores[j].id;
+  trEl.appendChild(tdEl);
+  for(var i= 0; i <= 15; i++) {
+    var cookieC = this.cookieCount();
+    cookiesDay += cookieC;
+    teEl = document.createElement('td');
+    var cookies = cookieC;
+    tdEl.textContent = ccokies;
+    trEl.appendChild(tdEl);
+  }
+  var dataTotal = document.createElement('td');
+  tdEl.textContent = cookiesDay;
+  tblEl.appendChild (dataTotal);
+};
